85b88dd931c507c4161761b85f3f407c967ac35f
Tobi Situ <tobisitu@gmail.com>
2019-10-16 17:12:01 -0400

    Updates

---
+++ precache-manifest.2f29cfcfd709f6befc0a2a5a596c9aa9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6e57647855ef0fb589ab0c1ca349d794",
+    "url": "/the-foodplace/index.html"
+  },
+  {
+    "revision": "8bd2693899adebe1293e",
+    "url": "/the-foodplace/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "502b45d1c7fe2da08b11",
+    "url": "/the-foodplace/static/js/2.f5a2183d.chunk.js"
+  },
+  {
+    "revision": "8bd2693899adebe1293e",
+    "url": "/the-foodplace/static/js/main.378dc4ba.chunk.js"
+  },
+  {
+    "revision": "65615dfc143bc007e3a3",
+    "url": "/the-foodplace/static/js/runtime-main.d9646abc.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/the-foodplace/static/media/logo.25bf045c.svg"
+  }
+]);
