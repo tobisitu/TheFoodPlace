0a44affc35165dfa8981e95e02686aba24ab4c30
Tobi Situ <tobisitu@gmail.com>
2019-10-22 21:47:30 -0400

    Completed landing page and footer. I am yet to complete the navigation bar

--- src/App.js
+++ src/App.js
@@ -1,17 +1,19 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Homepage from './components/homepage';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Footer from './components/footer';
+import Navigation from './components/navigation';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-         Hi! Welcome to the FoodPlace
-        </p>
-        
+        <h1 className="Masthead-text">The FoodPlace</h1>
+        <Navigation />
       </header>
+      <Homepage />
+      <Footer />
     </div>
   );
 }
